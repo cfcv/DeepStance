34e8b8a1d6d38e330898cc7f884a7dd65580b2c3
Cyril <brebisasthmatique@gmail.com>
2019-12-11 11:55:01 +0100

    Videos can now be read

--- DeepStance/mainwindow.cpp
+++ DeepStance/mainwindow.cpp
@@ -12,10 +12,17 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     scenesListModel_ = new QStringListModel();
 
+    isPlaying_ = false;
+
     ui->setupUi(this);
     ui->scenesListView->setModel(scenesListModel_);
 
+    QTimer *refreshTimer = new QTimer(this);
+    connect(refreshTimer, SIGNAL(timeout()), this, SLOT(readVideoNextFrame()));
+    refreshTimer->start(1/12);
+
     refreshScenesList();
+    loadSampleData();
 }
 
 MainWindow::~MainWindow()
@@ -37,7 +44,60 @@ void MainWindow::refreshScenesList()
     scenesListModel_->setStringList(sceneList);
 }
 
+void MainWindow::loadSampleData()
+{
+    std::ifstream sample("../db/test/v1.0-mini/sample_data.json");
+    sample >> SampleData_;
+}
+
+void MainWindow::findFirstImage(std::string token)
+{
+    currentIndex_ = 0;
+    qDebug() << SampleData_.size();
+    for(size_t i = 0; i < SampleData_.size(); i++) {
+        if(SampleData_[i]["sample_token"] == token) {
+            std::string fname = SampleData_[i]["filename"];
+            if(SampleData_[i]["fileformat"] == "jpg" && fname.find("CAM_FRONT")!= std::string::npos) {
+                currentIndex_ = i;
+                break;
+            }
+        }
+    }
+}
+
+void MainWindow::findNext()
+{
+    std::string nextToken = SampleData_[currentIndex_]["next"];
+
+    if(nextToken != "") {
+        for(size_t i = 0; i < SampleData_.size(); i++) {
+            std::string currToken = SampleData_[i]["token"];
+            if(currToken == nextToken) {
+                currentIndex_ = i;
+                break;
+            }
+        }
+    }
+}
+
+void MainWindow::setCurrentFrame()
+{
+    std::string fname = SampleData_[currentIndex_]["filename"];
+    ui->sfmlView->setBackgroundTexture("../db/test/" + fname);
+}
+
 void MainWindow::on_scenesListView_clicked(const QModelIndex &index)
 {
     ui->scenesDescriptionLabel->setText(QString::fromStdString(ScenesData_[index.row()]["description"]));
+    findFirstImage(ScenesData_[index.row()]["first_sample_token"]);
+    setCurrentFrame();
+    isPlaying_ = true;
+}
+
+void MainWindow::readVideoNextFrame()
+{
+    if(isPlaying_) {
+        findNext();
+        setCurrentFrame();
+    }
 }
--- DeepStance/mainwindow.h
+++ DeepStance/mainwindow.h
@@ -21,14 +21,24 @@ public:
 
     QStringListModel *scenesListModel_;
     json ScenesData_;
+    json SampleData_;
+
+    bool isPlaying_;
+
+    size_t currentIndex_;
 
 private slots:
     void on_scenesListView_clicked(const QModelIndex &index);
+    void readVideoNextFrame();
 
 private:
     Ui::MainWindow *ui;
 
     void refreshScenesList();
+    void loadSampleData();
+    void findFirstImage(std::string token);
+    void findNext();
+    void setCurrentFrame();
 };
 
 #endif // MAINWINDOW_H
